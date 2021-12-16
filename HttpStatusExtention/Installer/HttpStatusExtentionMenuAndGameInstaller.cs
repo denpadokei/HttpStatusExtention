69e0e65f8d5d3ffff03c6a7236d6ac80782ef3c7
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-12-17 01:40:49 +0900

    srmキューステータス送信を可能にした

---
+++ HttpStatusExtention/Installer/HttpStatusExtentionMenuAndGameInstaller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace HttpStatusExtention.Installer
+{
+    public class HttpStatusExtentionMenuAndGameInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            this.Container.BindInterfacesAndSelfTo<SRMQueueStatus>().FromNewComponentOnNewGameObject().AsCached().NonLazy();
+        }
+    }
+}
