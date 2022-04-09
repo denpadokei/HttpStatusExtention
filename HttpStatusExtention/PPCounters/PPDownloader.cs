87c756686e5275005fa01e56e1b2253c2004e89b
デンパ時計 <jsoldenpa@yahoo.co.jp>
2022-04-10 02:57:23 +0900

    #10 難易度が全部同じになってた ついでにV2.9カーブ対応

---
+++ HttpStatusExtention/PPCounters/PPDownloader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using Zenject;
+
+namespace HttpStatusExtention.PPCounters
+{
+    public class PPDownloader : IInitializable
+    {
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // プロパティ
+        public ReadOnlyDictionary<string, RawPPData> RowPPs { get; private set; }
+        public bool Init { get; private set; }
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // メンバ変数
+        private static readonly string s_uriPrefix = "https://cdn.pulselane.dev/";
+        private static readonly string s_fileName = "raw_pp.json";
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // 構築・破棄
+        public async void Initialize()
+        {
+            this.Init = false;
+            var result = await WebClient.GetAsync($"{s_uriPrefix}{s_fileName}", CancellationToken.None);
+            if (result == null || !result.IsSuccessStatusCode) {
+                return;
+            }
+            var token = result.ConvertToJToken();
+            this.RowPPs = new ReadOnlyDictionary<string, RawPPData>(token.ToObject<Dictionary<string, RawPPData>>());
+            this.Init = true;
+        }
+        #endregion
+    }
+}
---
+++ HttpStatusExtention/PPCounters/RawData.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace HttpStatusExtention.PPCounters
+{
+    public class RawPPData
+    {
+        [JsonProperty("_Easy_SoloStandard", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public float EasySoloStandard { get; set; }
+        [JsonProperty("_Normal_SoloStandard", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public float NormalSoloStandard { get; set; }
+        [JsonProperty("_Hard_SoloStandard", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public float HardSoloStandard { get; set; }
+        [JsonProperty("_Expert_SoloStandard", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public float ExpertSoloStandard { get; set; }
+        [JsonProperty("_ExpertPlus_SoloStandard", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public float ExpertPlusSoloStandard { get; set; }
+    }
+}
